549cedab9178498b594a549f9e903b7fb4dd645b
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-01-31 16:56:46 +0700

    3 - print text with a function

---
+++ 3-print-text-with-function.js
@@ -0,0 +1,16 @@
+// Print text with a function
+// Source: https://www.hackerrank.com/challenges/js10-hello-world/problem?isFullScreen=true
+// 31 January 2024
+
+// Function to print text
+// 1st method - basic function
+// function greeting(parameterVariable) {
+//     console.log('Hello, World!');
+
+//     console.log(parameterVariable);
+// }
+
+// 2nd method - Lambda function
+let greeting = (parameterVariable) => console.log(parameterVariable);
+
+greeting('I like nasi goreng!')
