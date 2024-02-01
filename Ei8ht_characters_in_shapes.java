e04813161787967563a3f1fb07cc0523870b24e3
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-02-01 22:50:51 +0700

    8 - characters in shapes

---
+++ Ei8ht_characters_in_shapes.java
@@ -0,0 +1,23 @@
+// TITLE: CHARACTERS IN SHAPE
+// TASK : Count the total characters of an array
+// Source: https://edabit.com/challenge/Py6fFcpDMJNWyTxRD
+// 1 February 2024
+
+public class Ei8ht_characters_in_shapes {
+
+	// 1st method - basic
+	// public static int countCharacters(String[] arr) {
+	// 	int totalCharacter = 0;
+	// 	for (String character : arr) {
+	// 		totalCharacter += character.length();
+	// 	}
+	// 	return totalCharacter;
+	// }
+
+	// 2nd method - more short
+	// public static int countCharacters(String[] arr) {
+	// 	int totalCharacter = 0;
+	// 	for (String characters : arr) totalCharacter += characters.length();
+	// 	return totalCharacter;
+	// }
+}
