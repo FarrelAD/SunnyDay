4456805116c8f4d01e24a6546e657f9e9e740c21
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-02-01 12:06:27 +0700

    5 - return first value array

---
+++ 5-return-first-elemen-array.js
@@ -0,0 +1,26 @@
+// Print text with a function
+// Source: https://edabit.com/challenge/QaApgtePE6QrCZ64o
+// 1 February 2024
+
+// Function to return first value of array
+// 1st method - basic function
+// function getFirstValue(arr) {
+// 	return arr[0]
+// }
+
+// 2nd method - lambda function
+// const getFirstValue = arr => arr[0]
+
+
+// 3rd method - inefficient method
+function getFirstValue(arr) {
+	let maxArrayLength = arr.length;
+	let firstValueOfArray = 0;
+	for (let i = maxArrayLength -1; i >= 0; i--) {
+		if (i == 0) {
+			return firstValueOfArray = arr[i]
+		}
+	}
+}
+
+console.log(getFirstValue([4, 5, 6, 800]))
