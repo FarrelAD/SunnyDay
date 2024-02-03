b2ae14d2c34ef3cd80729992c0073e37ef1c0029
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-02-03 12:09:21 +0700

    14 - query cities id

---
+++ 0014-query-cities-id.sql
@@ -0,0 +1,9 @@
+-- TITLE: QUERY CITIES ID
+-- TASK : Query all columns for a city in CITY with the ID 1661.
+-- Source: https://www.hackerrank.com/challenges/select-by-id/problem?isFullScreen=true
+-- 3 February 2024
+
+-- Below is the SQL code
+SELECT *
+FROM CITY
+WHERE ID = 1661
