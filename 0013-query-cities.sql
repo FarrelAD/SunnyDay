ca90b72dad76b84c54951ffad83a611926708214
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-02-03 12:06:37 +0700

    13 - query cities

---
+++ 0013-query-cities.sql
@@ -0,0 +1,8 @@
+-- TITLE: QUERY CITIES
+-- TASK : Query all columns (attributes) for every row in the CITY table.
+-- Source: https://www.hackerrank.com/challenges/select-all-sql/problem?isFullScreen=true
+-- 3 February 2024
+
+-- Below is the SQL code
+SELECT *
+FROM CITY
