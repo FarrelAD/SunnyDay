9f744b531d524b4d2fc7beac095eb2ff990059db
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-04-24 04:59:20 +0700

    87 - grasshoper - messi goals function

---
+++ 04-April-2024/0087-grasshoper-messi-goals-function.js
@@ -0,0 +1,15 @@
+// TITLE: GRASSHOPER - MESSI GOALS FUNCTION
+// TASK : Complete the function to return his total number of goals in all three leagues
+// Source: https://www.codewars.com/kata/55f73be6e12baaa5900000d4
+// 24 April 2024
+
+// 1st
+// function goals(laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+//     return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+// }
+
+// 2nd 
+const goals = (laLigaGoals, copaDelReyGoals, championsLeagueGoals) => laLigaGoals + copaDelReyGoals + championsLeagueGoals
+
+// Testing
+console.log(goals(43, 10, 5))
