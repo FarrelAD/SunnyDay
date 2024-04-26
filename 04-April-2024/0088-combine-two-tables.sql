38037f45840e89a47bb22d841a8ee9103eaf1354
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-04-26 07:59:53 +0700

    88 - [SQL] Combine two tables

---
+++ 04-April-2024/0088-combine-two-tables.sql
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/combine-two-tables/
+-- TITLE: COMBINE TWO TABLES
+-- TYPE: SQL, DATABASE
+-- TASK: Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+--       If the address of a personId is not present in the Address table, report null instead.
+--       Return the result table in any order.
+-- SOURCE: https://leetcode.com/problems/combine-two-tables
+-- 26 April 2024
+
+
+# Write your MySQL query statement below
+
+SELECT Person.firstName, Person.lastName, Address.city, Address.state
+FROM Person
+LEFT JOIN Address ON Person.personId = Address.personId
