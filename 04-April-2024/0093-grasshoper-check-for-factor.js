0dbc1d776007d042961945421f12d4bc846554be
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-05-06 08:08:45 +0700

    93 - grasshoper - check for factor

---
+++ 04-April-2024/0093-grasshoper-check-for-factor.js
@@ -0,0 +1,13 @@
+// TITLE: GRASSHOPER - CHECK FOR FACTOR
+// TASK : This function should test if the factor is a factor of base.
+//        Return true if it is a factor or false if it is not.
+// Source: https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+// 6 May 2024
+
+// 1st 
+function checkForFactor (base, factor) {
+    return base % factor  == 0
+}
+
+// Testing
+console.log(checkForFactor(2450,5))
