6b3db2d6ed77d43d4fd5a4d990756c6ad24774a9
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-05-10 08:16:04 +0700

    97 - classic hello world [sh]

---
+++ 05-May-2024/0097-classic-hello-world.sh
@@ -0,0 +1,18 @@
+#!bin/sh
+
+# TITLE: CLASSIC HELLO WORLD
+# TASK: You are given a method called main, make it print the line Hello World!, 
+#       (yes, that includes a new line character at the end) and don't return anything
+# SOURCE: https://www.codewars.com/kata/57036f007fd72e3b77000023
+# 10 May 2024
+
+
+function Solution
+{
+    function main
+    {
+        echo Hello World!
+    }
+    main
+}
+Solution    
