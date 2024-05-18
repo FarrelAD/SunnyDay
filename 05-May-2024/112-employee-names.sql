4cde19c0fa97a0450b16a5055378ec36c5747dd9
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-05-18 07:19:22 +0700

    112 - employee names [SQL]

---
+++ 05-May-2024/112-employee-names.sql
@@ -0,0 +1,9 @@
+-- TITLE: EMPLOYEE  NAMES
+-- TASK: Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+-- SOURCE: https://www.hackerrank.com/challenges/name-of-employees
+-- 18 May 2024
+
+
+SELECT name 
+FROM employee
+ORDER BY name ASC;
