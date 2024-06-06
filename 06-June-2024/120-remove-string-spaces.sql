8bf995f85084927f1c1fddec286f19b7692b99ed
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2024-06-07 06:55:58 +0700

    120 - remove string spaces [SQL]
    This is second SQL challenge in June

---
+++ 06-June-2024/120-remove-string-spaces.sql
@@ -0,0 +1,7 @@
+-- TITLE: REMOVE STRING SPACES
+-- TASK: Write a function that removes the spaces from the string, then return the resultant string.
+-- SOURCE: https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+-- 07 June 2024
+
+SELECT x, REPLACE(x, ' ', '') AS res
+FROM nospace;
